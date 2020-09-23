eda9e446866b716b5b07e0c421621b455b6f8fa3
pjupjupju <p.hill.erova@gmail.com>
2020-09-23 23:04:49 +0200

    feat(layout): basic flexbox skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
 import React from "react";
-import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-
         <Switch>
           <Route path="/">
             <Home />
           </Route>
         </Switch>
-      </div>
     </Router>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React from "react";
+import { Flex, Heading, Button } from "rebass";
 
-const Home = () => <h1>Welcome</h1>;
-
+const Home = () => (
+  <Flex
+    flexDirection="column"
+    justifyContent="center"
+    height="100%"
+    width="100%"
+    p="3"
+  >
+    <Flex sx={{ flexGrow: 1 }} justifyContent="center" flexDirection="column">
+      <Heading textAlign="center" color="white">
+        TIP FOR SCIENCE
+      </Heading>
+    </Flex>
+    <Flex
+      flexDirection="column"
+      sx={{
+        flexGrow: 0,
+        flexShrink: 0,
+      }}
+    >
+      <Flex mb="2">
+        <Button mr="1" flex="1">
+          about
+        </Button>
+        <Button ml="1" flex="1">
+          stats
+        </Button>
+      </Flex>
+      <Button>play</Button>
+    </Flex>
+  </Flex>
+);
 export { Home };
