862af6a1f441e6637654b492c043d7aa49f11f65
pjupjupju <p.hill.erova@gmail.com>
2020-12-07 21:31:08 +0100

    feat(Play): chart on submitted

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, KeyboardEvent } from 'react';
 import { Box, Flex, Heading, Image, Text } from 'rebass';
+import { ResponsiveLine } from '@nivo/line';
 import { Label, Input } from '@rebass/forms';
+import { mockData } from './data';
 
 interface Settings {
   question: string;
@@ -40,17 +42,19 @@ const labelStyle = {
 };
 
 const previousTipStyle = {
-  background: "pink",
+  background: '#FF0070',
   mr: 1,
   p: 1,
-}
+};
 
 const Game = ({
   settings: { question, image, previousTips, unit },
 }: GameProps) => {
   const [submitted, setSubmitted] = useState(false);
+  const [tip, setTip] = useState<number | null>(null);
   const handleSubmit = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
+      setTip(Number(event.currentTarget.value));
       setSubmitted(true);
     }
   };
@@ -69,7 +73,9 @@ const Game = ({
       <Image src={image} sx={imageStyle} />
       {previousTips != null && previousTips.length !== 0 && (
         <>
-          <Text textAlign="center" color="pink" mb={1}>Předchozí tipy: </Text>
+          <Text textAlign="center" color="#FF0070" mb={1}>
+            Předchozí tipy:{' '}
+          </Text>
           <Flex justifyContent="center" mb={1}>
             {previousTips.map((previousTip) => (
               <Text sx={previousTipStyle}>{previousTip}</Text>
@@ -105,7 +111,49 @@ const Game = ({
         </Heading>
       </Box>
       <Image src={image} sx={imageStyle} />
-      <Flex justifyContent="center" alignItems="center">
+      <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <Box width="100%" height="200px">
+          <ResponsiveLine
+            enableArea={true}
+            curve="natural"
+            xScale={{
+              type: 'linear',
+              min: 0,
+              max: 50,
+            }}
+            yScale={{
+              type: 'linear',
+              min: 0,
+              max: 1,
+            }}
+            data={mockData}
+            margin={{
+              top: 50,
+              right: 50,
+              bottom: 50,
+              left: 50,
+            }}
+            enableGridX={false}
+            enableGridY={false}
+            enablePoints={false}
+            markers={[
+              {
+                axis: 'x',
+                value: Number(tip),
+                lineStyle: { stroke: 'white', strokeWidth: 1 },
+                legend: 'váš tip',
+                textStyle: { fill: 'white' },
+              },
+              {
+                axis: 'x',
+                value: 18.29,
+                lineStyle: { stroke: '#FF0070', strokeWidth: 1 },
+                legend: 'správná odpověď',
+                textStyle: { fill: '#FF0070' },
+              },
+            ]}
+          />
+        </Box>
         <Text color="white">Tady bude graf a pod tím nějaký fun fact.</Text>
       </Flex>
     </Flex>
