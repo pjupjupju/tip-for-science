ffd68d87ecf550c2e54c70e2bc8d88e8eaea9b32
pjupjupju <p.hill.erova@gmail.com>
2021-03-30 21:11:26 +0200

    feat(db): database queries

--- src/server/schema/mutation/saveTip.ts
+++ src/server/schema/mutation/saveTip.ts
@@ -1,20 +1,51 @@
 import { DynamoDB } from 'aws-sdk';
-import { TABLE_TIP} from "./../../../config";
+import { TABLE_QUESTION, TABLE_TIP } from './../../../config';
 
 export async function saveTip(
   parent: any,
   { id, tip }: { id: string; tip: number },
-  {dynamoDB}: {dynamoDB: DynamoDB.DocumentClient}
+  { dynamoDB }: { dynamoDB: DynamoDB.DocumentClient }
 ) {
-  // TODO update tip table for ID with tip
   console.log(`--- updating Question ${id} with tip ${tip}`);
-  await dynamoDB.put({TableName: TABLE_TIP, Item: {
-    question_id: id,
-    tip,
-    user_id: 21,
-    created_at: new Date(),
-  }})
+  await dynamoDB.put({
+    TableName: TABLE_TIP,
+    Item: {
+      question_id: id,
+      tip,
+      user_id: 21,
+      created_at: new Date(),
+    },
+  });
 
+  // TODO: get next question id somehow
+  const nextQuestionId = 'flop';
+
+  const question = dynamoDB.query({
+    TableName: TABLE_QUESTION,
+    KeyConditionExpression: 'ID = :id',
+    ExpressionAttributeValues: { ':id': nextQuestionId },
+    ScanIndexForward: false,
+    Limit: 1,
+  });
+
+  const lastTips = dynamoDB.query({
+    TableName: TABLE_TIP,
+    KeyConditionExpression: 'question_id = :id',
+    ExpressionAttributeValues: { ':id': nextQuestionId },
+    ScanIndexForward: false,
+    Limit: 2,
+  }) as any;
+
+  await Promise.all([question, lastTips]);
+
+  return {
+    ...question,
+    previousTips: lastTips.map(
+      (previousTip: { tip: number }) => previousTip.tip
+    ),
+  };
+
+  /*
   return {
     id: '30b86d42-84aa-4ba7-9aa9-80b9c8f80cfa',
     question: 'How much is the fleeb?',
@@ -23,5 +54,5 @@ export async function saveTip(
     previousTips: [],
     correctAnswer: 15,
     unit: 'kg',
-  };
+  }; */
 }
