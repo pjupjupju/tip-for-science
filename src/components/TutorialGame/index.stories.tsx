529183e1fac4a89354ac7fa6777c6efec49358f0
pjupjupju <p.hill.erova@gmail.com>
2021-05-27 16:09:44 +0200

    feat(Game): game stories and refactor pt. X

---
+++ src/components/TutorialGame/index.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { TutorialGame } from './';
+
+export default { title: 'component/TutorialGame', component: TutorialGame };
+export const Default = () => <TutorialGame />;
+
+Default.storyName = 'Tutorial game';
