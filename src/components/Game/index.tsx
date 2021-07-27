88efc3316f47901624a42b9548eb6ceeb4dbc630
pjupjupju <p.hill.erova@gmail.com>
2021-07-28 00:14:39 +0200

    feat(Game): added ***ing submit button

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,4 +1,4 @@
-import React, { KeyboardEvent } from 'react';
+import React, { KeyboardEvent, useRef } from 'react';
 import { Box, Button, Flex, Image, Text } from 'rebass';
 import { ResponsiveLine } from '@nivo/line';
 import { Label, Input } from '@rebass/forms';
@@ -9,6 +9,7 @@ import { GameOverScreen } from './GameOverScreen';
 import { PreviousTips } from './PreviousTips';
 import { getScore } from '../../helpers';
 import { Container } from '../Container';
+import { SubmitButton } from '../SubmitButton';
 
 export interface Settings {
   question: string;
@@ -23,7 +24,7 @@ interface GameProps {
   isSubmitted: boolean;
   onFinish: Function;
   onHome: Function;
-  onSubmit?: (event: KeyboardEvent<HTMLInputElement>) => void;
+  onSubmit?: (tip: number) => void;
   settings: Settings;
   currentTip?: number;
   score: number;
@@ -63,11 +64,25 @@ const Game = ({
   currentTip,
   score,
 }: GameProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const handleClickFinish = () => {
     onFinish();
   };
   const handleClickHome = () => {
     onHome();
+  };
+
+  const handleSubmit = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && onSubmit) {
+      onSubmit(Number(event.currentTarget.value));
+    }
+  };
+
+  const handleClickSubmit = () => {
+    if (inputRef.current && onSubmit) {
+      onSubmit(Number(inputRef.current.value));
+    }
   };
 
   return !isSubmitted ? (
@@ -90,15 +105,17 @@ const Game = ({
           tip:
         </Label>
         <Input
+          ref={inputRef}
           id="tip"
           name="tip"
           type="number"
           placeholder="váš tip"
           sx={inputStyles}
-          onKeyDown={onSubmit}
+          onKeyDown={handleSubmit}
         />
         <Text color="white">{unit}</Text>
-      </Flex>
+      </Flex>{' '}
+      <Flex py={2} justifyContent="flex-end"><SubmitButton onClick={handleClickSubmit} timeLimit={timeLimit} /></Flex>
     </Container>
   ) : (
     <Container>
---
+++ src/components/SubmitButton/index.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { Box, Button, ButtonProps } from 'rebass';
+import { animated, useSpring } from 'react-spring';
+
+const Circle = ({ value }: { value: number }) => (
+  <Box
+    sx={{
+      width: '40px',
+      height: '40px',
+      borderRadius: '50%',
+      background: `conic-gradient(#000 0 ${value}%, transparent 0 100%)`,
+    }}
+  />
+);
+
+const AnimatedCircle =  animated(Circle);
+
+const AnimatedButton = ({ timeLimit, ...rest }: { timeLimit: number }) => {
+  const { value } = useSpring({
+    from: { value: 1 },
+    to: { value: 100 },
+    config: { duration: timeLimit * 1000 },
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('finished');
+    }, timeLimit * 1000);
+  }, [timeLimit]);
+  return (
+    <Button {...rest}>
+      <AnimatedCircle
+        value={value}
+      />
+    </Button>
+  );
+};
+
+const SubmitButton = ({ timeLimit, ...rest }: ButtonProps & { timeLimit?: number }) =>
+  typeof timeLimit !== 'undefined' ? (
+    <AnimatedButton {...rest} timeLimit={timeLimit} />
+  ) : (
+    <Button {...rest}>button</Button>
+  );
+
+export { SubmitButton };
