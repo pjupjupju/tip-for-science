e98f515b5835a999d8601a29178dc39cdff6015f
pju <p.hill.erova@gmail.com>
2022-08-02 02:00:07 +0200

    feat(Game): improved game over/no more questions visuals

---
+++ src/components/NoMoreQuestions/index.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NoMoreQuestions } from './';
+
+export default {
+  title: 'components/NoMoreQuestions',
+  component: NoMoreQuestions,
+};
+
+export const Default = () => <NoMoreQuestions score={161} />;
+Default.storyName = 'No more questions';
