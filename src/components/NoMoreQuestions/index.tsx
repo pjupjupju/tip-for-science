815a6c7fe788486a7a059b9c1ab2641ce9b2f8cb
pju <p.hill.erova@gmail.com>
2022-08-04 16:07:24 +0200

    feat(NoMoreQuestions): add link to profile

--- src/components/NoMoreQuestions/index.tsx
+++ src/components/NoMoreQuestions/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Container } from '..';
 import { useHistory } from 'react-router';
 import { TranslucentBox } from '../TranslucentBox';
+import { Link } from 'react-router-dom';
 
 interface NoMoreQuestionsProps {
   score: Number;
@@ -18,51 +19,54 @@ const NoMoreQuestions = ({ score }: NoMoreQuestionsProps) => {
   };
 
   return (
-    <TranslucentBox>
-      <Container>
-        <Flex
-          p={3}
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-          width="100%"
-          height="100%"
+    <Container>
+      <Flex
+        p={3}
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        width="100%"
+        height="100%"
+      >
+        <Text
+          fontSize={4}
+          fontWeight={500}
+          textAlign="center"
+          my={2}
+          color="secondary"
         >
-          <Text
-            fontSize={4}
-            fontWeight={500}
-            textAlign="center"
-            my={2}
-            color="secondary"
-          >
-            Toto byla (prozatím) poslední otázka.
-            <br />
-            Tvoje skóre:{' '}
-          </Text>
-          <Text color="accent" as="span" fontSize={6} fontWeight={700}>
-            {score}
-          </Text>
-          <Text fontWeight={500} textAlign="center" my={5} color="secondary">
-            Až budou k dispozici další, dáme ti vědět na registrační e-mail.
-            <br />Zkontrolovat nebo opravit jej můžeš v sekci "Profil".
-          </Text>
+          Toto byla (prozatím) poslední otázka.
+          <br />
+          Tvoje skóre:{' '}
+        </Text>
+        <Text color="accent" as="span" fontSize={6} fontWeight={700}>
+          {score}
+        </Text>
+        <Text fontWeight={500} textAlign="center" my={5} color="secondary">
+          Až budou k dispozici další, dáme ti vědět na registrační e-mail.
+          <br />
+          Zkontrolovat nebo opravit jej můžeš v sekci{' '}
+          <Link to="/profile" style={{ color: '#FF0070' }}>
+            Profil
+          </Link>
+          .
+        </Text>
 
-          <Flex justifyContent="space-between" width="100%" mt={6}>
-            <Button
-              onClick={handleClickHome}
-              sx={{ flex: 2 }}
-              mr={1}
-              backgroundColor={'#414141'}
-            >
-              Domů
-            </Button>
-            <Button onClick={handleClickProfile} sx={{ flex: 2 }}>
-              Profil
-            </Button>
-          </Flex>
+        <Flex justifyContent="space-between" width="100%" mt={6}>
+          <Button
+            onClick={handleClickHome}
+            sx={{ flex: 2 }}
+            mr={1}
+            backgroundColor={'#414141'}
+          >
+            Domů
+          </Button>
+          <Button onClick={handleClickProfile} sx={{ flex: 2 }}>
+            Profil
+          </Button>
         </Flex>
-      </Container>
-    </TranslucentBox>
+      </Flex>
+    </Container>
   );
 };
 
