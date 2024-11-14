ea0e06a71a83aa27adf455b95feb8da1e0b57f67
pew <p.hill.erova@gmail.com>
2024-11-15 00:25:28 +0100

    feat(Translations): added score messages bcs someone is dumbo

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,5 @@
 export * from './gameLogic';
 export * from './generateQuestionBundle';
-export * from './getScoreSentence';
 export * from './isServer';
 export * from './sliceIntoChunks';
 export * from './useYupValidationResolver';
