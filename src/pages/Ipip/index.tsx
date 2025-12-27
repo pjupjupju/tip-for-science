ed61bc3e4357a33231371d7e27f7710ab308af5c
pew <p.hill.erova@gmail.com>
2025-12-27 12:28:53 +0100

    feat(Ipip): created ipip component & page (they just exist, they can't do much 🥺)

---
+++ src/pages/Ipip/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './../../components/Container';
+import { Questionnaire } from '../../components/Questionnaire';
+
+const Ipip = () => {
+  return (
+    <Container>
+      <Questionnaire />
+    </Container>
+  );
+};
+
+export { Ipip };
